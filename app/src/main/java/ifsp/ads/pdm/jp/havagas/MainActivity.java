f5061cd329a158ae4037739e20ecb4b2250d3c84
kpi-jps <jps.spj@gmail.com>
2022-08-21 19:15:53 -0300

    under construction

--- app/src/main/java/ifsp/ads/pdm/jp/havagas/MainActivity.java
+++ app/src/main/java/ifsp/ads/pdm/jp/havagas/MainActivity.java
@@ -3,6 +3,8 @@ package ifsp.ads.pdm.jp.havagas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 import ifsp.ads.pdm.jp.havagas.databinding.ActivityMainBinding;
 
@@ -13,5 +15,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
+
+        activityMainBinding.mobilePhoneCb.setOnClickListener( view -> {
+                activityMainBinding.mobilePhoneEt.setVisibility(View.GONE);
+                activityMainBinding.monographTitleEt.setText("");
+                if(activityMainBinding.mobilePhoneCb.isChecked())
+                    activityMainBinding.mobilePhoneEt.setVisibility(View.VISIBLE);
+            }
+        );
+
+        activityMainBinding.educationSp.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        activityMainBinding.monographTitleEt.setVisibility(View.GONE);
+                        activityMainBinding.monographTitleEt.setText("");
+                        activityMainBinding.advisorEt.setVisibility(View.GONE);
+                        activityMainBinding.advisorEt.setText("");
+                        if (i >= 3) activityMainBinding.institutionEt.setVisibility(View.VISIBLE);
+                        if (i >= 4) {
+                            activityMainBinding.monographTitleEt.setVisibility(View.VISIBLE);
+                            activityMainBinding.advisorEt.setVisibility(View.VISIBLE);
+                        }
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) { }
+                }
+        );
+
+
+    }
+
+    private void showFormData() {
+
+    }
+
+    private void resetForm() {
+        activityMainBinding.nameEt.setText("");
+        activityMainBinding.phoneEt.setText("");
+        activityMainBinding.emailEt.setText("");
+        activityMainBinding.emailListCb.setChecked(true);
+        activityMainBinding.birthDateEt.setText("");
+        activityMainBinding.femaleRb.setChecked(true);
+        activityMainBinding.mobilePhoneCb.setChecked(false);
+        activityMainBinding.mobilePhoneEt.setText("");
+        activityMainBinding.institutionEt.setText("");
+        activityMainBinding.conclusionEt.setText("");
+        activityMainBinding.monographTitleEt.setText("");
+        activityMainBinding.educationSp.setSelection(0);
+        activityMainBinding.mobilePhoneEt.setVisibility(View.GONE);
+        activityMainBinding.advisorEt.setVisibility(View.GONE);
+        activityMainBinding.monographTitleEt.setVisibility(View.GONE);
     }
 }
